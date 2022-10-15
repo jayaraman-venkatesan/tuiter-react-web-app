477de3ca10a65bc034beec8bcba883aba12e9892
Jayaraman Venkatesan <jayaramanvenkatesan@Jayaramans-MacBook-Air.local>
2022-10-15 12:23:52 -0400

    Assign - 5

---
+++ public/tuiter/WhoToFollowList/who.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        avatarIcon: '../assets/java.png',
+        userName: 'Java', handle: 'Java'
+    },
+    {
+        avatarIcon: '../assets/virgin-1.svg',
+        userName: 'Virgin Galactic',
+        handle: 'virgingalactic',
+    },
+    {
+        avatarIcon: '../assets/nasa.jpg',
+        userName: 'NASA', handle: 'NASA',
+    },
+];
